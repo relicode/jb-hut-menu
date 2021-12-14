9d13ad1f8b39fd910c0861008e6f5271130b020b
Anssi Herranen <mccall@iki.fi>
2021-12-14 11:47:01 +0700

    Add basic image functionality

--- src/components/MenuSection/index.tsx
+++ src/components/MenuSection/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 import styles from './styles.module.css'
 import { BAHT, MenuItem } from '../../menu'
+import defaultImage from '../../assets/img/logo__256x256.png'
 
 const ARROW_DOWN = '\u25BC'
 const ARROW_UP = '\u25B2'
@@ -19,11 +20,19 @@ const MenuSection: React.FC<MenuSectionProps> = (props): JSX.Element => {
       <header className={styles.header} onClick={() => { setOpen(!open) }}>
         {title} {open ? ARROW_UP : ARROW_DOWN}
       </header>
-      {open && menuItems.map(([, name, price]) => (
+      {open && menuItems.map(([, name, price, imageBaseName]) => (
         <div
           key={[title, name].join('/')}
         >
-          <div className={styles.itemName}>{name}</div>
+          <div className={styles.itemImage}>
+            <img src={imageBaseName
+              ? `${process.env.PUBLIC_URL}/img/menu/${imageBaseName}__256x256.jpg`
+              : defaultImage
+            }/>
+          </div>
+          <div className={styles.itemName}>
+            {name}
+          </div>
           <div className={styles.itemPrice}>
             {`${price}${BAHT}`}
           </div>
