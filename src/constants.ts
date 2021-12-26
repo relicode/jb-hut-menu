d97ba89f99ff42fd17c5b137d977beb0b857ff44
Anssi Herranen <mccall@iki.fi>
2021-12-26 22:37:05 +0700

    Use thumbnail images as placeholders while loading

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const BAHT = '฿' as const
+export const SMALL_IMAGE_LENGTH = 256 as const
+export const LARGE_IMAGE_LENGTH = 1024 as const
+export const SMALL_IMAGE_SIZE = `${SMALL_IMAGE_LENGTH}x${SMALL_IMAGE_LENGTH}` as const
+export const LARGE_IMAGE_SIZE = `${LARGE_IMAGE_LENGTH}x${LARGE_IMAGE_LENGTH}` as const
